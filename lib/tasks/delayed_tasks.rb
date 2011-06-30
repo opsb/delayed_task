cd9707cbc11eef7e4854bea344f03d651684f91b
opsb <oliver@opsb.co.uk>
2011-06-30 10:12:29 +0100

    Include Rake::DSL to avoid deprecation warnings

--- lib/tasks/delayed_tasks.rb
+++ lib/tasks/delayed_tasks.rb
@@ -1,10 +1,13 @@
 module DelayedTask
-  def self.add_delayed_tasks
-    Rake::Task.tasks.each do |task|
-      task "delay:#{task.name}" do
-        Rake::Task["environment"].invoke
-        Delayed::Job.enqueue DelayedTask::PerformableTask.new(task.name)    
-        puts "Enqueued job: rake #{task.name}"
+  class << self
+    include Rake::DSL
+    def add_delayed_tasks
+      Rake::Task.tasks.each do |task|
+        task "delay:#{task.name}" do
+          Rake::Task["environment"].invoke
+          Delayed::Job.enqueue DelayedTask::PerformableTask.new(task.name)    
+          puts "Enqueued job: rake #{task.name}"
+        end
       end
     end
   end
